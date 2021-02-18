f9e143ac4ca2a2b1683076d2671929d26b448095
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-19 00:31:26 +0530

    Pin Code UC1

--- MainClass.java
+++ MainClass.java
@@ -5,22 +5,22 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class MainClass {
-	
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Enter Pin Code : ");
 		String pinCode = sc.nextLine();
-		Pattern pt = Pattern.compile("^[1-9][0-9]{5}$");
+		Pattern pt = Pattern.compile("[0-9]{6}");
 		Matcher mt = pt.matcher(pinCode);
 
 		boolean result = mt.matches();
-		
+
 		if(result)
-            System.out.println("Valid Pin");
-        else    
-            System.out.println("Invalid Pin");
+			System.out.println("Valid Pin");
+		else    
+			System.out.println("Invalid Pin");
 
 	}
 
