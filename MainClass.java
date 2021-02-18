2820cdddf3958cbb916ba496eba459d247f486e7
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-18 20:41:21 +0530

    Pin Code UC1

--- MainClass.java
+++ MainClass.java
@@ -1,9 +1,26 @@
 package com.blbz;
 
-public class MainClass {
+import java.util.Scanner;
 
+public class MainClass {
+	
+	public static final int maxDigit = 6;
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Pin Code : ");
+		int userPinCode = sc.nextInt();
+		int counter = 0;
+		
+		while(userPinCode > 0)
+		{
+			userPinCode = userPinCode / 10;
+			counter++;
+		}
+		
+		if(counter == maxDigit)
+			System.out.println("Valid Pin");
+		else
+			System.out.println("Invalid Pin");
 
 	}
 
