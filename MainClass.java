60142c7b8d80a71f45fba16367fccd7deeb83da5
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-19 00:13:39 +0530

    Pin code UC1

--- MainClass.java
+++ MainClass.java
@@ -1,26 +1,23 @@
 package com.blbz;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class MainClass {
 
-	public static final int maxDigit = 6;
+
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
+
 		System.out.print("Enter Pin Code : ");
-		int userPinCode = sc.nextInt();
-		int counter = 0;
+		String pinCode = sc.nextLine();
+		Pattern pt = Pattern.compile("^[1-9][0-9]{5}$");
+		Matcher mt = pt.matcher(pinCode);
 
-		while(userPinCode > 0)
-		{
-			userPinCode = userPinCode / 10;
-			counter++;
-		}
-
-		if(counter == maxDigit)
-			System.out.println("Valid Pin");
-		else
-			System.out.println("Invalid Pin");
+		boolean result = mt.matches();
+		System.out.println(result);
 
 	}
 
